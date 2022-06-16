2fcbc158c63dd260d74200a06e68931d7a940c35
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-06-16 15:05:43 -0600

    Concepto de one way data binding - enlazado en una sola vía

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -12,11 +12,19 @@ export class HeroeComponent{
   edad: number = 21;
   funcion: string = "Salvar vidas"
 
-  get nombreCapitalizado(){
+  get nombreCapitalizado():string {
     return this.nombre.toUpperCase();
   }
 
   obtenerNombre(): string{
       return `${this.nombre} - ${this.edad}`;
+  }
+
+  cambiarNombre(): void{
+    this.nombre = "SpiderMan";
+  }
+
+  cambiarEdad(): void{
+    this.edad = 22;
   }
 }
