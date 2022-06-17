0d5561fd5790f883533c3456a61388f136d632bd
DenisRodMor <104526631+DenisRodMor@users.noreply.github.com>
2022-06-17 09:29:10 -0600

    Uso del  ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre:string;
+  poder:number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent  {
 
+  nuevo:Personaje={
+    nombre: "MrSatán",
+    poder: 100
+  }
+
+  cambiarNombre(event : any){
+    console.log(event);
+
+  }
 
   agregar(){
-    console.log("Hey!!!!")
+    console.log(this.nuevo);
   }
 }
